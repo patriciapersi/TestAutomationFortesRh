756eb53539651a58d4400c764e6ee549831f290f
Eliú Alves Araújo <53905432+Eliu-Alves@users.noreply.github.com>
2023-05-08 16:51:29 -0300

    Caso de Teste Avaliacao aluno (#47)
    * Caso de Teste Avaliacao aluno
    
    * ajuste descriçao do arquivo do teste
    
    ---------
    
    Co-authored-by: Patricia Persi <persipatricia@gmail.com>

--- cypress/e2e/6 - Desenvolvimento de talento/AvaliacoesDosAlunos.cy.js
+++ cypress/e2e/6 - Desenvolvimento de talento/AvaliacoesDosAlunos.cy.js
@@ -1,14 +1,17 @@
 describe('Avaliações dos Alunos', () => {
 
     const dados = {
-        tituloAV: chance.name(),
+        tituloAV: chance.sentence({ words: 2 }),
         tipoAV: chance.character({ pool: 'pn' }),
         minimoAprov: chance.integer({ min: 0, max: 100.000 }),
-        tituloAVManual: chance.name(),
-        tituloModelo: chance.sentence({ words: 2 })
+        tituloAVManual: chance.sentence({ words: 2 }),
+        tituloModelo: chance.sentence({ words: 2 }),
+        colaborador: chance.name(),
+        cpf: chance.cpf().split(/[.\-]/).join(''),
+        curso: chance.word(),
     }
     const dados2 = {
-        tituloAV: chance.name(),
+        tituloAV: chance.sentence({ words: 2 }),
         tipoAV: 'a',
         minimoAprov: chance.integer({ min: 0, max: 100.000 }),
         tituloAVManual: chance.name(),
@@ -20,6 +23,7 @@ describe('Avaliações dos Alunos', () => {
             .insereAvaliacaodosAlunos(dados)
             .insereModeloAvaliacaodosAlunos(dados)
             .insereAvaliacaodosAlunoscomModelo(dados2)
+            .insereColaborador(dados)
             .navigate('/desenvolvimento/avaliacao-curso')
     });
 
@@ -70,6 +74,24 @@ describe('Avaliações dos Alunos', () => {
             .generalButtons('Remover', dados2.tituloAV) 
             .popUpMessage('Confirma exclusão?')
             .validaMensagem('Avaliação excluída com sucesso.')
+    })
+
+    it('Tenta excluir uma Avaliação com Resposta', () => {
+        cy
+            .insereCurso(dados.curso)
+        cy  
+            .exec_sql(
+                "insert into colaboradorturma(id, aprovado, colaborador_id, prioridadetreinamento_id, turma_id, curso_id, cursolnt_id, motivoreprovacao)values (1, false, 1, null, 1, 1, null, 'REPROVADO')",
+                "insert into colaboradorquestionario(id, colaborador_id, questionario_id, respondida, respondidaem, turma_id, candidato_id, avaliacao_id, performance, observacao, avaliacaodesempenho_id, avaliador_id, solicitacao_id, avaliacaocurso_id, performancenivelcompetencia, configuracaonivelcompetenciacolaborador_id, pesoavaliador, respondidaparcialmente, respostasintegradascolabore) values (1, 1, null, true, '12/04/2023', 1, null, 10001, null, null, null, null, null, 2, null, null, 1, false, false)",
+                "insert into colaboradorresposta(id, comentario, valor, pergunta_id, resposta_id, colaboradorquestionario_id, areaorganizacional_id, estabelecimento_id, cargo_id, dia, integrada, colaboradorquestionariohash, respondidaparcialmente) values (1, 'Teste', null, 3, null, 1, 1, 1, null, null, false, null, false)",
+            )
+        cy
+            .generalButtons('Remover', dados2.tituloAV)   
+            .popUpMessage('Confirma exclusão?')
+            .validaMensagem('Entidade avaliação de aluno possui dependências em:')
+        cy  
+            .contains(dados2.tituloAV).should('be.visible')
+            
     })
 
 })
