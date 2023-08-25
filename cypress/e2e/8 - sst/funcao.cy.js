57f7df0b26f4aa8633cd7868f7289e7c0b236e2c
Patricia Persi <83375003+patriciapersi@users.noreply.github.com>
2023-08-25 13:43:15 -0300

    refactor funcao popup mensagem (#83)
    Co-authored-by: Eliu-Alves <eliu1084@gmail.com>

--- cypress/e2e/8 - sst/funcao.cy.js
+++ cypress/e2e/8 - sst/funcao.cy.js
@@ -1,20 +1,20 @@
 import * as returnDate from '../../support/functions'
 describe('Cadastro de Funções', () => {
 
-     const dados = {
-         nome_funcao: chance.word({ syllables: 4 }),
-         funcao: chance.word({ syllables: 3 }),
-         descricaoFuncao: chance.paragraph({ sentences: 1 }),
-         obraNome: chance.name(),
-         nomeRisco: chance.word(),
-         caepfTeste: '81868475026081',
-         cnpjTeste: '27259575000180',
-         cnoTeste: '207238696781',
-         nomeEpiRisco: chance.word() ,
-         nomeEpiRisco2 : chance.word(),
-         nomeRiscoScript: chance.word(),
+    const dados = {
+        nome_funcao: chance.word({ syllables: 4 }),
+        funcao: chance.word({ syllables: 3 }),
+        descricaoFuncao: chance.paragraph({ sentences: 1 }),
+        obraNome: chance.name(),
+        nomeRisco: chance.word(),
+        caepfTeste: '81868475026081',
+        cnpjTeste: '27259575000180',
+        cnoTeste: '207238696781',
+        nomeEpiRisco: chance.word(),
+        nomeEpiRisco2: chance.word(),
+        nomeRiscoScript: chance.word(),
     }
-    
+
 
     beforeEach('', () => {
         cy
@@ -24,13 +24,13 @@ describe('Cadastro de Funções', () => {
             .inserirRisco(dados)
             .navigate('/sst/funcoes')
     })
-  
+
     it('Inserir Função', () => {
 
         cy.cadastraFuncao(dados)
         cy.validaMensagem(`Função "${dados.nome_funcao}" cadastrada com sucesso.`)
         cy.contains(dados.funcao).should('have.css', 'color', "rgb(0, 128, 0)")
-        
+
 
     });
 
@@ -41,31 +41,33 @@ describe('Cadastro de Funções', () => {
         cy
             .contains('Hoje').should('be.visible').trigger('mouseouver').click()
         cy
-            .digita('#nomeFuncao',dados.nome_funcao)
-            .digita('input[name="historicoFuncao.codigoCBO"]','Analista')
+            .digita('#nomeFuncao', dados.nome_funcao)
+            .digita('input[name="historicoFuncao.codigoCBO"]', 'Analista')
         cy
             .contains('252510 - Analista de câmbio').should('be.visible').click()
         cy
-            .digita('textarea[name="historicoFuncao.descricao"]', dados.descricaoFuncao, {delay:0})
-            
+            .digita('textarea[name="historicoFuncao.descricao"]', dados.descricaoFuncao, { delay: 0 })
+
         cy
             .contains('label', 'EPIs (PPRA)').should('be.visible').click()
             .get('.checklistbox-header').eq(1).within(($form) => {
                 cy.contains('button', 'Marcar Todos').should('be.visible').click({ force: true })
             })
-            
+
         cy
             .contains('td', dados.nomeRisco).parent()
             .find('.p-checkbox-box').should('be.visible').click()
-        
+
         cy
             .contains('td', dados.nomeRisco).parent()
             .find('.fa-list').should('be.visible').click()
-        cy.contains('.epi-nome-item', dados.nomeEpiRisco )
+        cy.contains('.epi-nome-item', dados.nomeEpiRisco)
         cy.contains('span', 'Sim').next().should('be.visible').click()
         cy.contains('li', 'Não').should('be.visible').click({ force: true })
-        
-            .clickNewButton('OK')
+
+          .clickNewButton('OK')
+        cy.contains('.p-dialog', 'Foram identificados EPIS com eficácias divergentes.')
+        cy.clickNewButton('OK')
         cy
             .contains('td', dados.nomeRisco).parent()
             .find('span', 'Selecione').next().should('be.visible').click()
@@ -76,53 +78,53 @@ describe('Cadastro de Funções', () => {
             .contains(`Função "${dados.nome_funcao}" cadastrada com sucesso.`).should('be.visible')
         cy  
             .contains(dados.funcao).should('have.css', 'color', "rgb(0, 128, 0)")
-         
+
     });
 
-    
+
     it('Editar função - Historico cadastral', () => {
         cy
-            .generalButtons('Históricos Cadastrais',  dados.funcao)   
-        cy            
+            .generalButtons('Históricos Cadastrais', dados.funcao)
+        cy
             .generalButtons('Editar', dados.funcao)
         cy
             .get('.fa-calendar-alt').should('not.be.enabled')
-            .digita('textarea[name="descricao"]',dados.descricaoFuncao)
+            .digita('textarea[name="descricao"]', dados.descricaoFuncao)
             .clickNewButton('Gravar')
             .validaMensagem('Histórico da função atualizado com sucesso.')
-        cy 
+        cy
             .contains('td', dados.funcao).parent()
             .find('.fa-trash').should('not.be.enabled').click()
 
     });
 
-    it('Editar função - Historico gerencial', () => { 
-            cy
-                .generalButtons('Históricos Gerenciais',  dados.funcao)   
-            cy            
-                .generalButtons('Editar', '01/05/2020')
-            cy
-                .contains('label','A função possui atribuição de comando, chefia, coordenação, supervisão ou gerência.').should('be.visible').click()
-                .clickNewButton('Gravar')
-                .validaMensagem('Histórico gerencial da função atualizada com sucesso.')
+    it('Editar função - Historico gerencial', () => {
+        cy
+            .generalButtons('Históricos Gerenciais', dados.funcao)
+        cy
+            .generalButtons('Editar', '01/05/2020')
+        cy
+            .contains('label', 'A função possui atribuição de comando, chefia, coordenação, supervisão ou gerência.').should('be.visible').click()
+            .clickNewButton('Gravar')
+            .validaMensagem('Histórico gerencial da função atualizada com sucesso.')
     });
 
     it('Edita função com data de historico gerencial não permitida', () => {
         cy
-           .generalButtons('Históricos Gerenciais', 'Assistente')
-       cy
-           .generalButtons('Editar', '01/04/2020')
-       cy
-           .contains('label', 'EPIs (PPRA)').should('be.visible').click()
-           .get('.checklistbox-header').eq(1).within(($form) => {
-               cy.contains('button', 'Marcar Todos').should('be.visible').click({ force: true })
-           })
-        cy  
-           .clickNewButton('Gravar')
-           .validaMensagem('Não é permitido que o histórico gerencial da função tenha data menor que o primeiro histórico cadastral da função. Data do primeiro histórico cadastral: 01/05/2020')
-      
+            .generalButtons('Históricos Gerenciais', 'Assistente')
+        cy
+            .generalButtons('Editar', '01/04/2020')
+        cy
+            .contains('label', 'EPIs (PPRA)').should('be.visible').click()
+            .get('.checklistbox-header').eq(1).within(($form) => {
+                cy.contains('button', 'Marcar Todos').should('be.visible').click({ force: true })
+            })
+        cy
+            .clickNewButton('Gravar')
+            .validaMensagem('Não é permitido que o histórico gerencial da função tenha data menor que o primeiro histórico cadastral da função. Data do primeiro histórico cadastral: 01/05/2020')
 
-   })
+
+    })
 
 
     it('Inativar Função', () => {
@@ -130,14 +132,14 @@ describe('Cadastro de Funções', () => {
         cy
             .generalButtons('Inativar', dados.funcao)
         cy
-            .contains(`Deseja inativar a função "${dados.funcao}"`) 
+            .contains(`Deseja inativar a função "${dados.funcao}"`)
             .clickNewButton('OK')
-        cy  
+        cy
             .contains(dados.funcao).should('have.css', 'color', "rgb(255, 0, 0)")
 
     })
     it('Excluir funcao', () => {
-        cy         
+        cy
             .generalButtons('Excluir', dados.funcao)
             .popUpMessage('Confirma exclusão?')
             .validaMensagem('Função excluída com sucesso.')
