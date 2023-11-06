c5ed56946dfb3f61d38c506cf38c336bd08870d1
Patricia Persi <83375003+patriciapersi@users.noreply.github.com>
2023-11-06 13:33:00 -0300

    valida maxlength (#91)
    * valida maxlength
    
    * Revisão
    
    ---------
    
    Co-authored-by: Eliu-Alves <eliu1084@gmail.com>

--- cypress/e2e/8 - sst/condicoesAmbientais.cy.js
+++ cypress/e2e/8 - sst/condicoesAmbientais.cy.js
@@ -1,24 +1,24 @@
 import * as returnDate from '../../support/functions'
 describe('Condições ambientais', () => {
 
-     const dados = {
-         nomeProfissional: chance.word({ syllables: 4 }),
-         dataIni: '21/09/2022',
-         nomeGHE: chance.word({syllables: 6 }),
-         colaborador: chance.name(),
-         ambiente: chance.word({syllables: 7 }),
-         cpf: chance.cpf().split(/[.\-]/).join(''),
-         funcao: chance.word({ syllables: 6 }),
-         descricao: chance.sentence({ words: 15 }),
-         nomeRiscoScript: chance.word()
-     }
-    
-     const dados2 ={
+    const dados = {
+        nomeProfissional: chance.word({ syllables: 4 }),
+        dataIni: '21/09/2022',
+        nomeGHE: chance.word({ syllables: 6 }),
+        colaborador: chance.name(),
+        ambiente: chance.word({ syllables: 7 }),
+        cpf: chance.cpf().split(/[.\-]/).join(''),
+        funcao: chance.word({ syllables: 6 }),
+        descricao: chance.sentence({ words: 15 }),
+        nomeRiscoScript: chance.word()
+    }
+
+    const dados2 = {
         gravidade_nome: chance.word()
-        
-     }
-     
-    
+
+    }
+
+
 
     beforeEach('', () => {
         cy
@@ -31,10 +31,10 @@ describe('Condições ambientais', () => {
             .inserirRisco(dados)
             .navigate('/sst/condicao-ambientais')
     })
-  
+
     it('Inserir Condição ambiental - sem agente nocivo', () => {
 
-        
+
         cy
             .clickNewButton('Inserir')
             .get('input[name="data"').clear().type(returnDate.formatDate(new Date(), 0))
@@ -43,7 +43,7 @@ describe('Condições ambientais', () => {
             .get('.p-dropdown-items').within(($form) => {
                 cy.contains('li', dados.nomeGHE).should('be.visible').click({ force: true })
             })
-        
+
         cy
             .contains('label', 'Talentos que estão no ambiente e/talvez na função do Grupo Homogêneo selecionado: *').should('be.visible').click()
             .get('.checklistbox-header').last().within(($form) => {
@@ -57,15 +57,16 @@ describe('Condições ambientais', () => {
     it('Editar Condição ambiental - inserir agente nocivo 09.01.001', () => {
 
         cy
-             .contains('td', dados.colaborador).parent()
-             .find('.fa-edit').should('be.visible').click()    
-        cy   .contains('Inserir Agentes Nocivos').should('be.visible').click()
-        cy   .contains('.p-fieldset-content', dados.colaborador) 
-        cy   .contains('label', 'Agente nocivo:*').next().click()
-             .get('.p-dropdown-items').within(($form) => {
-            cy
-                .contains('li', '09.01.001').click({ force: true })
-            }) 
+            .contains('td', dados.colaborador).parent()
+            .find('.fa-edit').should('be.visible').click()
+        cy.get('textarea[name = "descricaoAtividades"]').should('have.attr', 'maxLength', '999')
+        cy.contains('Inserir Agentes Nocivos').should('be.visible').click()
+        cy.contains('.p-fieldset-content', dados.colaborador)
+        cy.contains('label', 'Agente nocivo:*').next().click()
+            .get('.p-dropdown-items').within(($form) => {
+                cy
+                    .contains('li', '09.01.001').click({ force: true })
+            })
             .clickNewButton('Gravar')
             .validaMensagem('Agentes Nocivos aos Quais o Trabalhador Está Exposto salvo com sucesso.')
             .clickNewButton('Gravar')
@@ -75,52 +76,52 @@ describe('Condições ambientais', () => {
     it('Excluir condição ambiental', () => {
         cy
             .contains('td', dados.colaborador).parent()
-            .find('.fa-trash').should('be.visible').click()       
+            .find('.fa-trash').should('be.visible').click()
             .popUpMessage('Confirma exclusão?')
             .validaMensagem('Condição ambiental excluído com sucesso.')
     });
 
     it('Validar o Numero CA quando inserimos 2 EPIs com Codigos Iguais', () => {
-      
+
         cy
             .contains('td', dados.colaborador).parent()
-            .find('.fa-edit').should('be.visible').click()    
-        cy  
+            .find('.fa-edit').should('be.visible').click()
+        cy
             .contains('Inserir Agentes Nocivos').should('be.visible').click()
-        cy  
-            .contains('.p-fieldset-content', dados.colaborador) 
-        cy  
+        cy
+            .contains('.p-fieldset-content', dados.colaborador)
+        cy
             .contains('label', 'Agente nocivo:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
-            cy
-                .contains('li', '03.01.007').click({ force: true })
+                cy
+                    .contains('li', '03.01.007').click({ force: true })
             })
-        cy  
+        cy
             .contains('label', 'Risco:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
-            cy
-                .contains('li', dados.nomeRiscoScript).click({ force: true })
+                cy
+                    .contains('li', dados.nomeRiscoScript).click({ force: true })
             })
-        cy  
+        cy
             .contains('label', 'Gravidade do risco:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
-            cy
-                .contains('li', 'Leve').click({ force: true })
+                cy
+                    .contains('li', 'Leve').click({ force: true })
             })
-        cy  
+        cy
             .contains('label', 'Probabilidade do risco:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
-            cy
-                .contains('li', 'Altamente Exposto').click({ force: true })
+                cy
+                    .contains('li', 'Altamente Exposto').click({ force: true })
             })
-        cy  
+        cy
             .contains('label', 'Tipo da avaliação: *').next().click()
             .get('.p-dropdown-items').within(($form) => {
-            cy
-                .contains('li', 'Critério qualitativo').click({ force: true })
+                cy
+                    .contains('li', 'Critério qualitativo').click({ force: true })
             })
-        
-       cy  
+
+        cy
             .get('#rh_id_25 > .p-dropdown-label').click()
             .get('[aria-label="Utilizado"]').click()
             .get('#rh_id_26').click()
@@ -141,47 +142,47 @@ describe('Condições ambientais', () => {
             .insereGravidadeRiscos(dados2.gravidade_nome)
             .reload()
         cy
-            .generalButtons('Editar', dados.colaborador)    
-        cy   
+            .generalButtons('Editar', dados.colaborador)
+        cy
             .contains('Inserir Agentes Nocivos').should('be.visible').click()
-        cy   
-            .contains('.p-fieldset-content', dados.colaborador) 
-        cy   
+        cy
+            .contains('.p-fieldset-content', dados.colaborador)
+        cy
             .contains('label', 'Agente nocivo:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
                 cy
-                .contains('li', '03.01.007').click({ force: true })
-                })
-        cy   
+                    .contains('li', '03.01.007').click({ force: true })
+            })
+        cy
             .contains('label', 'Risco:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
                 cy
-                .contains('li', dados.nomeRiscoScript).click({ force: true })
-                }) 
-        cy   
+                    .contains('li', dados.nomeRiscoScript).click({ force: true })
+            })
+        cy
             .contains('label', 'Gravidade do risco:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
                 cy
-                .contains('li', dados2.gravidade_nome).click({ force: true })
-                }) 
-        cy   
+                    .contains('li', dados2.gravidade_nome).click({ force: true })
+            })
+        cy
             .contains('label', 'Probabilidade do risco:*').next().click()
             .get('.p-dropdown-items').within(($form) => {
                 cy
-                .contains('li', 'Altamente Exposto').click({ force: true })
-                }) 
-        cy   
+                    .contains('li', 'Altamente Exposto').click({ force: true })
+            })
+        cy
             .contains('label', 'Tipo da avaliação: *').next().click()
             .get('.p-dropdown-items').within(($form) => {
                 cy
-                .contains('li', 'Critério qualitativo').click({ force: true })
-                }) 
+                    .contains('li', 'Critério qualitativo').click({ force: true })
+            })
 
             .clickNewButton('Gravar')
             .clickNewButton('OK')
-            
+
         cy
-            .contains('Classificação de severidade do risco é obrigatória. O resultado dessa Classificação de Severidade de Risco vem da combinação entre Gravidade do Risco e Probabilidade do Risco.').should('have.css', 'color', "rgb(244, 67, 54)") 
+            .contains('Classificação de severidade do risco é obrigatória. O resultado dessa Classificação de Severidade de Risco vem da combinação entre Gravidade do Risco e Probabilidade do Risco.').should('have.css', 'color', "rgb(244, 67, 54)")
 
     });
 
